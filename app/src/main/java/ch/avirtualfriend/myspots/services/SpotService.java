80a345b9e755af5399405e722dfe1516966ba5f6
migrosdata <antoine.induni@gmail.com>
2019-03-13 22:16:52 +0100

    Add service

---
+++ app/src/main/java/ch/avirtualfriend/myspots/services/SpotService.java
@@ -0,0 +1,11 @@
+package ch.avirtualfriend.myspots.services;
+
+import java.util.Collection;
+
+import ch.avirtualfriend.myspots.models.Spot;
+
+public class SpotService {
+    public Collection<Spot> getAllSpots() {
+        return null;
+    }
+}
