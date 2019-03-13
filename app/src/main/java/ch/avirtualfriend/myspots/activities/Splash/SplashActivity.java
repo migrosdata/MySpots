48b00d251054365bf440eb8b64e9f644348c5ec7
migrosdata <antoine.induni@gmail.com>
2019-03-13 18:21:23 +0100

    Add splash screen

---
+++ app/src/main/java/ch/avirtualfriend/myspots/activities/Splash/SplashActivity.java
@@ -0,0 +1,41 @@
+package ch.avirtualfriend.myspots.activities.Splash;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+import ch.avirtualfriend.myspots.R;
+import ch.avirtualfriend.myspots.activities.Main.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    VideoView videoView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        startVideo();
+    }
+
+    private void startVideo() {
+        videoView = (VideoView) findViewById(R.id.videoView);
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.splash);
+        videoView.setVideoURI(video);
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                startNextActivity();
+            }
+        });
+        videoView.start();
+    }
+
+    private void startNextActivity() {
+        if (isFinishing()) {
+            return;
+        }
+        startActivity(new Intent(this, ch.avirtualfriend.myspots.activities.Main.MainActivity.class));
+        finish();
+    }
+}
---
+++ app/src/main/java/ch/avirtualfriend/myspots/activities/Spots/SpotsActivity.java
@@ -0,0 +1,15 @@
+package ch.avirtualfriend.myspots.activities.Spots;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ch.avirtualfriend.myspots.R;
+
+public class SpotsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spots);
+    }
+}
