a358ac0efc194a7b55163c0480322fef9eea61f5
keezy27 <inf.carmine@gmail.com>
2019-05-02 21:32:19 +0200

    zoom set to 17 map type = GoogleMap.MAP_TYPE_SATELLITE

--- app/src/main/java/ch/avirtualfriend/myspots/services/Location/SpotLocationListener.java
+++ app/src/main/java/ch/avirtualfriend/myspots/services/Location/SpotLocationListener.java
@@ -3,11 +3,14 @@ package ch.avirtualfriend.myspots.services.Location;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.zip.ZipOutputStream;
 
 import ch.avirtualfriend.myspots.models.Spot;
 
@@ -30,7 +33,20 @@ public class SpotLocationListener implements LocationListener {
         LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
         googleMap.addMarker(new MarkerOptions().position(myLocation)
                 .title("Ici"));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+      /*  googleMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+
+
+            @Override
+            public void onCameraIdle() {
+                float zoomLevel = googleMap.getCameraPosition().zoom;
+                String zoom = String.valueOf(zoomLevel);
+                Log.d("DEBBUG", zoom);
+            }
+        });*/
+
     }
 
     @Override
